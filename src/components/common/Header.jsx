b3d2c4855b3f4f829a59de3f795f9dbcd867dbe6
o12356 <dhtkdqhd9115@gmail.com>
2022-11-05 20:00:10 +0900

    metamask 연동

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import styles from "./header.module.css";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "../../lib/connectors";
 
 function Header() {
+  const { chainedId, account, active, activate, deactivate } = useWeb3React();
+
+  const connectWallet = async () => {
+    try {
+      await activate(injected, (error) => {
+        // 크롬 익스텐션 없을 경우 오류 핸들링
+      });
+    } catch (err) {}
+  };
+
+  console.log(active);
   return (
     <div className={styles.headerContainer}>
       <div className={styles.innerContainer}>
@@ -13,9 +26,19 @@ function Header() {
         <Link to="/campaign" className={styles.menu}>
           캠페인
         </Link>
-        <Button href="#" variant="outlined" className={styles.loginBtn}>
-          로그인
-        </Button>
+        {active ? (
+          <Button variant="outlined" className={styles.loginBtn}>
+            마이페이지
+          </Button>
+        ) : (
+          <Button
+            variant="outlined"
+            className={styles.loginBtn}
+            onClick={connectWallet}
+          >
+            로그인
+          </Button>
+        )}
       </div>
     </div>
   );
